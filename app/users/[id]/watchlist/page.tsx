f4338a50ad278095200296c6bd64f6996e020406
MatiasRoje <rojechi@gmail.com>
2023-09-30 22:51:09 +0200

    user routed protected if user is not authenticated

--- app/users/[id]/watchlist/page.tsx
+++ app/users/[id]/watchlist/page.tsx
@@ -1,4 +1,17 @@
+"use client";
+
+import { useAuth } from "@/contexts/AuthContext";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 function UserWatchlistPage() {
+  const router = useRouter();
+  const { isAuthenticated } = useAuth();
+
+  useEffect(() => {
+    if (!isAuthenticated) router.push("/login");
+  }, [isAuthenticated, router]);
+
   return <div>Watchlist</div>;
 }
 
